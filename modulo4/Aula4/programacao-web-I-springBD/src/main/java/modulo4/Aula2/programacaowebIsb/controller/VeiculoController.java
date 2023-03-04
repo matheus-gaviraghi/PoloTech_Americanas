f8f04d3701be72d37bb487a7ba25c71716bc149c
matheus-gaviraghi <matheus.gaviraghi@acad.ufsm.br>
2023-03-03 21:53:52 -0300

    Thymeleaf Create e Read CRUD

--- modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/controller/VeiculoController.java
+++ modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/controller/VeiculoController.java
@@ -3,58 +3,36 @@ package modulo4.Aula2.programacaowebIsb.controller;
 import modulo4.Aula2.programacaowebIsb.model.Veiculo;
 import modulo4.Aula2.programacaowebIsb.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping("/veiculo")
+@Controller
 public class VeiculoController {
 
     @Autowired
     private VeiculoService veiculoService;
 
-//    @PostMapping("/") // verbo requisitado para o padrão REST
-//    // Poderia usar o @RequestMapping(method = RequestMethod.POST, path="/"), seria a mesma coisa
-//    public void createVeiculo(@RequestBody Veiculo veiculo){
-//        this.veiculoService.createVeiculo(veiculo);
-//    }
-//    // Se não colocarmos a anotação RequestBody teremos a criação de objetos vazios
-
-    @PostMapping("/")
-    public ResponseEntity<String> createVeiculo(@RequestBody Veiculo veiculo){
-        try{
-            this.veiculoService.createVeiculo(veiculo);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body("Veículo criado!");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    @GetMapping("/veiculos")
+    public String veiculos(Model model){
+        List<Veiculo> veiculos = this.veiculoService.listarTodos();
+        model.addAttribute("veiculos", veiculos);
+        return "veiculos";
     }
 
-    @GetMapping("/todos")
-    public List<Veiculo> listarTodos(){
-        return this.veiculoService.listarTodos();
+    @GetMapping("/veiculo/add")
+    public String addVeiculo(Model model){
+        model.addAttribute("veiculo", new Veiculo());
+        return "veiculo-add";
     }
 
-    @GetMapping("/by/{id}")
-    public ResponseEntity<Veiculo> buscarVeiculoPorId(@PathVariable("id") Long id){
-
-        Optional<Veiculo> optionalVeiculo = this.veiculoService.buscarPorVeiculoId(id);
-
-        if(optionalVeiculo.isPresent()){
-            return ResponseEntity.ok(optionalVeiculo.get());
-        }
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @PutMapping("/")
-    public void atualizarVeiculo(){
-
+    @PostMapping("/veiculo/add")
+    public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo){
+        this.veiculoService.createVeiculo(veiculo);
+        return "redirect:/veiculos";
     }
 }
