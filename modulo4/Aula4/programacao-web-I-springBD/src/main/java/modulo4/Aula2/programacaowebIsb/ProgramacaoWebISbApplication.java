6ece9d304037b1dda00329eb0e0b1aa2b92f745f
matheus-gaviraghi <matheus.gaviraghi@acad.ufsm.br>
2023-03-08 19:41:07 -0300

    Criando várias telas Thymeleaf

--- modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/ProgramacaoWebISbApplication.java
+++ modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/ProgramacaoWebISbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgramacaoWebISbApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProgramacaoWebISbApplication.class, args);
 	}
 
--- modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/controller/VeiculoController.java
+++ modulo4/Aula4/programacao-web-I-springBD/src/main/java/modulo4/Aula2/programacaowebIsb/controller/VeiculoController.java
@@ -5,11 +5,10 @@ import modulo4.Aula2.programacaowebIsb.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class VeiculoController {
@@ -26,6 +25,7 @@ public class VeiculoController {
 
     @GetMapping("/veiculo/add")
     public String addVeiculo(Model model){
+        model.addAttribute("add", Boolean.TRUE);
         model.addAttribute("veiculo", new Veiculo());
         return "veiculo-add";
     }
@@ -33,6 +33,32 @@ public class VeiculoController {
     @PostMapping("/veiculo/add")
     public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo){
         this.veiculoService.createVeiculo(veiculo);
+        return "redirect:/veiculos";
+    }
+
+    @GetMapping("/veiculo/{veiculoId}/delete")
+    // usamos o metodo GET pela forma como implementamos o delete direto na tabela
+    // poderiamos usar o @DeleteMapping se criassemos um Pop-up com formulario que passasse
+    // o metodo DELETE, e daí funcionaria. Mas a interface web passa um método GET da forma como
+    // foi implementada, e por isso temos que deixar como GetMapping
+    public String deletarVeiculo(@PathVariable("veiculoId") Long veiculoId){
+        this.veiculoService.removerVeiculoPorId(veiculoId);
+        return "redirect:/veiculos";
+    }
+
+    @GetMapping("/veiculo/{veiculoId}/edit")
+    public String mostrarEdicaoVeiculo(Model model, @PathVariable("veiculoId") Long veiculoId){
+        Optional<Veiculo> optionalVeiculo = this.veiculoService.buscarPorVeiculoId(veiculoId);
+        optionalVeiculo.ifPresent(veiculo -> model.addAttribute("veiculo", veiculo));
+        model.addAttribute("add", Boolean.FALSE);
+        return "veiculo-add";
+    }
+
+    @PutMapping("/veiculo/{veiculoId}/edit")
+    public String editarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo,
+                                @PathVariable("veiculoId") Long veiculoId){
+        veiculo.setId(veiculoId);
+        this.veiculoService.createVeiculo(veiculo); // se ja existe na base, ele faz update
         return "redirect:/veiculos";
     }
 }
