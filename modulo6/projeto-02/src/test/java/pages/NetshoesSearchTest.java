2ae78feb3267e28dc28b4b0e2aca27de6282992c
matheus-gaviraghi <matheus.gaviraghi@acad.ufsm.br>
2023-04-25 20:21:23 -0300

    Teste de pesquisa de item implementado

--- modulo6/projeto-02/src/test/java/pages/NetshoesSearchTest.java
+++ modulo6/projeto-02/src/test/java/pages/NetshoesSearchTest.java
@@ -2,7 +2,13 @@ package pages;
 
 import br.com.ada.pages.NetshoesSearch;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,5 +26,29 @@ public class NetshoesSearchTest {
         String title = homePage.getTitulo();
         String expected = "Loja de Artigos Esportivos Online | Netshoes";
         assertEquals(expected, title);
+    }
+
+    @Test
+    public void testePesquisaItem(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mathe\\Downloads\\selenium\\chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        ChromeDriver driver = new ChromeDriver();
+
+        NetshoesSearch homePage = new NetshoesSearch(driver);
+
+        String textoPesquisado = "camiseta do Grêmio";
+
+        homePage.pesquisarPor(textoPesquisado);
+
+        ExpectedCondition condicaoEsperada = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                System.out.println("Item pesquisado (LowerCase): " + textoPesquisado.toLowerCase());
+                System.out.println("Título recebido (LowerCase): " + driver.getTitle().toLowerCase());
+                return driver.getTitle().toLowerCase().startsWith(textoPesquisado.toLowerCase());
+            }
+        };
+        new WebDriverWait(driver, Duration.ofMillis(500)).until(condicaoEsperada);
+
     }
 }
